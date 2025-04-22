f3fffa23ffb34ed5fc155741c4a1aa3f01b60302
Keith Packard <keithp@keithp.com>
2025-04-21 22:15:28 -0700

    gcc: Support Canadian cross builds on microblazeel
    Picolibc uses the linker during the build process to construct crt0.o
    files, but the microblazeel spec from gcc adds -dT xilinx.ld when no
    -T argument is provided to introduce default linking
    parameters. However, during a Canadian cross build, the xilinx.ld file
    isn't installed before picolibc gets built causing the build to fail.
    
    Fix this by not using xilinx.ld when the -r flag is selected.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- gcc/config/microblaze/microblaze.h
+++ gcc/config/microblaze/microblaze.h
@@ -110,7 +110,7 @@ extern enum pipeline_type microblaze_pipe;
   %{Zxl-mode-xmdstub:-defsym _TEXT_START_ADDR=0x800} \
   %{mxl-mode-xmdstub:-defsym _TEXT_START_ADDR=0x800} \
   %{mxl-gp-opt:%{G*}} %{!mxl-gp-opt: -G 0} \
-  %{!T*: -dT xilinx.ld%s}"
+  %{!r:%{!T*: -dT xilinx.ld%s}}"
 
 /* Specs for the compiler proper  */
 
